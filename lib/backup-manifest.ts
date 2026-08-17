be590ce8d68e7d2f69f6e04765f7bb6a980ca678
getmte512 <getmte512@gmail.com>
2026-08-16 22:35:57 -0500

    Add authenticated CRM backup export

---
+++ lib/backup-manifest.ts
@@ -0,0 +1,2 @@
+export const backupTables=["contacts","outreach_drafts","sales_pipeline","shopify_customer_links","shopify_sync_runs","reorder_requests","audit_events"] as const;
+export function buildBackupSummary(data:Record<string,unknown[]>){return Object.fromEntries(backupTables.map(table=>[table,data[table]?.length??0]));}
